831b04d3ed3817af482eea8de267e0b622460723
Serhat Teker <serhat.teker@gmail.com>
2022-03-25 14:37:22 +0300

    Add id to UserSerializer

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -7,4 +7,4 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("username", "name")
+        fields = ("id", "username", "name")
