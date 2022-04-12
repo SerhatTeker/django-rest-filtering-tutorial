616a876ca6445017079e35b3c79176bce56b4e9f
Serhat Teker <serhat.teker@gmail.com>
2022-04-13 01:28:32 +0300

    Add create for ArticleSerializer nesting

--- src/articles/serializers.py
+++ src/articles/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from src.articles.models import Article
+from src.regions.models import Region
 from src.regions.serializers import RegionSerializer
 
 
@@ -10,3 +11,12 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ("id", "author", "title", "content", "regions")
+
+    def create(self, validated_data):
+        regions_data = validated_data.pop("regions")
+        article = Article.objects.create(**validated_data)
+        for data in regions_data:
+            region, _ = Region.objects.get_or_create(**data)
+            article.regions.add(region)
+
+        return article
